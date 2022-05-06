381c286ba39419c1e7aaf50eb0d976e3f2f54b86
rossrobino <rbrobino@gmail.com>
2022-05-06 13:38:31 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	import Nav from './components/Nav.svelte';
+	let title = 'Backtracker';
+</script>
+
+<header>
+	<Nav {title}/>
+</header>
+
+<main>
+	<table>
+		<tr>
+			<td>Ticker:</td>
+			<td><input type="text"></td>
+		</tr>
+		<tr>
+			<td>Principle:</td>
+			<td><input type="number" min=0 ></td>
+		</tr>
+		<tr>
+			<td>Start Date:</td>
+			<td><input type="date"></td>
+		</tr>
+		<tr>
+			<td>End Date:</td>
+			<td><input type="date"></td>
+		</tr>
+	</table>
+</main>
+
+<footer>
+
+</footer>
+
+<style>
+
+	@media (min-width: 640px) {
+
+	}
+</style>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let title;
+</script>
+
+<nav>
+    <h1>{title}</h1>
+</nav>
+
+
+<style>
+    h1 {
+        color: green;
+    }
+</style>
